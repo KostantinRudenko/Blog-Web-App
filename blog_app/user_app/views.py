7b80619790b8e45817c31bca39b895541f5b4b7c
KostantinRudenko <rudenkokostya20@gmail.com>
2025-01-24 09:28:08 +0200

    added styles and html code to login template

--- blog_app/user_app/views.py
+++ blog_app/user_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def login(request):
-    return render(request, 'user_app/login.html')
+    context = {"title" : "Login"}
+    return render(request, 'user_app/login.html', context)
 
 def signup(request):
     return render(request, 'user_app/signup.html')
