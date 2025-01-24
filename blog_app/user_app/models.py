0412f69212214ac66a1b598b3fc84b60034d2857
KostantinRudenko <rudenkokostya20@gmail.com>
2025-01-24 08:13:01 +0200

    migrate user model

--- blog_app/user_app/models.py
+++ blog_app/user_app/models.py
@@ -1,6 +1,19 @@
 from django.db import models
-
 from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 class User(AbstractUser):
-    pass
+    description = models.TextField(max_length=512, blank=True, null=True)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='user_app_users',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='user_app_users',
+        blank=True
+    )
+    
+    class Meta:
+        pass
