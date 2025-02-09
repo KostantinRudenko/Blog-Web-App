daccce1afc1e6ff7b75272436e1560993054a7ef
KostantinRudenko <rudenkokostya20@gmail.com>
2025-02-09 09:41:08 +0200

    added db for comments

--- blog_app/post_app/models.py
+++ blog_app/post_app/models.py
@@ -8,3 +8,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.CharField(max_length=150)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
