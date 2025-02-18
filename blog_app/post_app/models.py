8ca62fd67f2b813da89782e96f33a8596fe63472
KostantinRudenko <rudenkokostya20@gmail.com>
2025-02-18 10:35:38 +0200

    added creating of comments

--- blog_app/post_app/models.py
+++ blog_app/post_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Post(models.Model):
+    id = models.IntegerField(auto_created=True, primary_key=True)
     title = models.CharField(max_length=150, unique=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,7 +11,7 @@ class Post(models.Model):
         return self.title
 
 class Comments(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.CharField(max_length=150)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- blog_app/post_app/views.py
+++ blog_app/post_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 from .models import Post, Comments
+from .forms import CreateCommentForm
 # Create your views here.
 
 def index(request):
@@ -18,8 +19,20 @@ def contacts(request):
 
 def post(request, title):
     post = Post.objects.get(title=title)
+    form = CreateCommentForm()
+    if request.method == "POST":
+        form = CreateCommentForm(data=request.POST)
+        if form.is_valid():
+            username = request.user.username
+            content = form.cleaned_data["content"]
+            Comments.objects.create(user=username, post=post, content=content)
+            return HttpResponseRedirect(request.META["HTTP_REFERER"])
+    else:
+        form = CreateCommentForm()
+    
     comments = Comments.objects.filter(post=post)
     context = {"title": "Post",
                "post": post,
-               "comments": comments}
+               "comments": comments,
+               "form": form}
     return render(request, 'post.html', context)
