42ee45f98e3212798c81efb2f600f93e4ad85d8f
KostantinRudenko <rudenkokostya20@gmail.com>
2025-03-07 21:05:09 +0200

    deleted user_app_user table(it wasn't used)

--- blog_app/user_app/models.py
+++ blog_app/user_app/models.py
@@ -1,19 +1,1 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
 # Create your models here.
-class User(AbstractUser):
-    description = models.TextField(max_length=512, blank=True, null=True)
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='user_app_users',
-        blank=True
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='user_app_users',
-        blank=True
-    )
-    
-    class Meta:
-        pass
